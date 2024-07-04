7f59294030efade211c4b8b0d79104e52c07107a
caipei <fancaipei@gmail.com>
2024-07-04 14:14:39 +0800

    home header

--- web/src/pages/home/components/homeHeader.js
+++ web/src/pages/home/components/homeHeader.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { getAvatarImg } from "../../../utils/user";
+
 const HomeHeader = () => {
-  return <div>HOME HEADER</div>;
+  const [kidAvatar, setKidAvatar] = useState(() => {
+    return getAvatarImg("giraffe");
+  });
+
+  return (
+    <div className='px-5 h-full flex items-center'>
+      <img src={kidAvatar} className='w-8 h-8' />
+      <span className='ml-2 text-lg'>Kid1</span>
+    </div>
+  );
 };
 
 export default HomeHeader;
---
+++ web/src/utils/user.js
@@ -0,0 +1,5 @@
+const getAvatarImg = (avatarCode) => {
+  return `${window.location.origin}/assets/images/avatars/${avatarCode}.png`;
+};
+
+export { getAvatarImg };
